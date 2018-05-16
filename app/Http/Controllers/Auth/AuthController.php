165a6f42ee78fa31b954cb5fcbfa7fb2d2cd8323
foundnation12 <foundnation12@ez.web.ne.jp>
2018-05-16 10:21:31 +0000

    user login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -29,6 +29,7 @@ class AuthController extends Controller
      * @return void
      */
     protected $redirectTo = '/';
+    protected $loginPath = '/login';    // 追加
     
     public function __construct()
     {
